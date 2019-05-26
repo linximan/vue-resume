3ea31bac9a360db866822e9590e9e904f597c4d8
Jax Chu <294242589@qq.com>
2019-05-26 22:20:28 +0800

    demo page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import BasicLayout from "./components/BasicLayout.vue";
 import Home from "./views/Home.vue";
+import DemoCRM from "./views/DemoCRM.vue";
 
 Vue.use(Router);
 
@@ -15,6 +16,11 @@ export default new Router({
           path: "/",
           name: "Home",
           component: Home
+        },
+        {
+          path: "/demo/crm",
+          name: "DemoCRM",
+          component: DemoCRM
         }
       ]
     }
---
+++ src/views/DemoCRM.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="demo-section">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ name: 'Home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>CRM 销售信息管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="carousel-container">
+      <h3 class="sub-title">预览</h3>
+      <el-carousel :interval="4000" height="320px">
+        <el-carousel-item v-for="item in 6" :key="item">
+          <h3 class="medium">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="tips-container">
+      <h3 class="sub-title">技术要点</h3>
+      <ul class="tips-list">
+        <li>1.XXX</li>
+        <li>2.XXX</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style type="text/css" lang="less" scoped>
+.demo-section {
+  padding: 20px;
+}
+.sub-title {
+  font-size: 18px;
+  margin: 16px 0;
+}
+
+.carousel-container {
+  padding: 20px;
+  // width: 640px;
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+  }
+}
+
+.tips-container {
+  padding: 20px;
+  .tips-list {
+    list-style: none;
+    line-height: 2;
+    font-size: 14px;
+  }
+}
+</style>
